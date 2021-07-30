7fe268b81323f9062d5673b63700ba1abcbf7f58
rigormorrtiss <rigormorrtiss@gmail.com>
2021-07-30 22:13:59 +0400

    Change default theme to Dracula

--- util/theme.go
+++ util/theme.go
@@ -6,26 +6,30 @@ import (
 )
 
 type Theme struct {
-	DropDownBackground              string `json:"dropdown.background"`
+	DropDownBackground   string `json:"dropdown.background"`
+	TreeViewBackground   string `json:"treeview.background"`
+	TextViewBackground   string `json:"textview.background"`
+	InputFieldBackground string `json:"inputField.background"`
+
 	DropDownForeground              string `json:"dropdown.foreground"`
-	InputFieldBackground            string `json:"inputField.background"`
+	TextViewForeground              string `json:"textview.foreground"`
+	TreeNodeForeground              string `json:"treenode.foreground"`
 	InputFieldForeground            string `json:"inputField.foreground"`
 	InputFieldPlaceholderForeground string `json:"inputField.placeholderTextForeground"`
-	TreeViewBackground              string `json:"treeview.background"`
-	TextViewBackground              string `json:"textview.background"`
-	TextViewForeground              string `json:"textview.foreground"`
 }
 
 func NewTheme() *Theme {
 	var theme Theme
-	theme.TextViewBackground = "#2E3440"
-	theme.TextViewForeground = "#D8DEE9"
-	theme.TreeViewBackground = "#2E3440"
-	theme.InputFieldBackground = "#3B4252"
-	theme.InputFieldForeground = "#D8DEE9"
-	theme.InputFieldPlaceholderForeground = "#D8DEE9"
 	theme.DropDownBackground = "#3B4252"
-	theme.DropDownForeground = "#D8DEE9"
+	theme.TreeViewBackground = "#282a36"
+	theme.TextViewBackground = "#282a36"
+	theme.InputFieldBackground = "#3B4252"
+
+	theme.DropDownForeground = "#f8f8f2"
+	theme.TextViewForeground = "#f8f8f2"
+	theme.TreeNodeForeground = "#8be9fd"
+	theme.InputFieldForeground = "#f8f8f2"
+	theme.InputFieldPlaceholderForeground = "#6272a4"
 
 	userHomeDir, err := os.UserHomeDir()
 	if err != nil {
