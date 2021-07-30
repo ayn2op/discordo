d684312f38c69bbbdfa35f35477b8e51f2aa7c7e
rigormorrtiss <rigormorrtiss@gmail.com>
2021-07-30 20:37:13 +0400

    Implement ChannelsTreeView Component for categories (#7)
    * Add ChannelsTreeView component
    
    * Remove ChannelsList component
    
    * Implement ChannelsTreeView comp for categories
    
    * Update README preview

--- ui/flex.go
+++ ui/flex.go
@@ -4,20 +4,14 @@ import (
 	"github.com/rivo/tview"
 )
 
-func NewMainFlex(guildsDropDown *tview.DropDown, channelsList *tview.List, messagesTextView *tview.TextView) (mainFlex *tview.Flex) {
-	midFlex := NewMidFlex(channelsList, messagesTextView)
+func NewMainFlex(guildsDropDown *tview.DropDown, channelsTreeView *tview.TreeView, messagesTextView *tview.TextView) (mainFlex *tview.Flex) {
+	midFlex := tview.NewFlex().
+		AddItem(channelsTreeView, 20, 1, false).
+		AddItem(messagesTextView, 0, 3, false)
 	mainFlex = tview.NewFlex().
 		SetDirection(tview.FlexRow).
 		AddItem(guildsDropDown, 3, 1, false).
 		AddItem(midFlex, 0, 1, false)
-
-	return
-}
-
-func NewMidFlex(channelsList *tview.List, messagesTextView *tview.TextView) (midFlex *tview.Flex) {
-	midFlex = tview.NewFlex().
-		AddItem(channelsList, 20, 1, false).
-		AddItem(messagesTextView, 0, 3, false)
 
 	return
 }
