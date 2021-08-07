1902f5908d11b7d6140a8eb59ade0eb93593c6f9
rigormorrtiss <rigormorrtiss@gmail.com>
2021-08-07 18:40:02 +0400

    fix(main): compare CategoryID against 0 and discord.NullChannelID for 'global channels'

--- discordo.go
+++ discordo.go
@@ -188,7 +188,7 @@ func onGuildsDropDownSelected(_ string, i int) {
 			channelNode.SetColor(tcell.GetColor(config.Theme.TreeNodeForeground))
 			channelsTreeNode.AddChild(channelNode)
 		case discord.GuildText, discord.GuildNews:
-			if channel.CategoryID == discord.NullChannelID {
+			if channel.CategoryID == 0 || channel.CategoryID == discord.NullChannelID {
 				channelNode.SetText("[::d]#" + channel.Name + "[-:-:-]")
 				channelsTreeNode.AddChild(channelNode)
 			}
