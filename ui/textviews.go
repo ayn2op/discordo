044e48da2e7a2a2f6936c5a6d3d4477f1db54efa
rigormorrtiss <rigormorrtiss@gmail.com>
2021-08-10 22:34:37 +0400

    refactor: move event handlers to ui package

--- ui/textviews.go
+++ ui/textviews.go
@@ -6,17 +6,23 @@ import (
 	"github.com/rivo/tview"
 )
 
-func NewMessagesTextView(onMessagesTextViewChanged func(), theme *util.Theme) *tview.TextView {
+func NewMessagesTextView(app *tview.Application, theme *util.Theme) *tview.TextView {
 	messagesTextView := tview.NewTextView()
 
 	messagesTextView.
 		SetDynamicColors(true).
 		SetWordWrap(true).
 		ScrollToEnd().
-		SetChangedFunc(onMessagesTextViewChanged).
+		SetChangedFunc(onMessagesTextViewChanged(app)).
 		SetBackgroundColor(tcell.GetColor(theme.TextViewBackground)).
 		SetBorder(true).
 		SetBorderPadding(0, 0, 1, 1)
 
 	return messagesTextView
+}
+
+func onMessagesTextViewChanged(app *tview.Application) func() {
+	return func() {
+		app.Draw()
+	}
 }
