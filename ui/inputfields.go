ba793f3afd126e754166f6e31ad6e93417fed017
rigormorrtiss <rigormorrtiss@gmail.com>
2021-08-11 22:42:19 +0400

    main: integrate user guilds in TreeView

--- ui/inputfields.go
+++ ui/inputfields.go
@@ -1,9 +1,6 @@
 package ui
 
 import (
-	"strings"
-
-	"github.com/atotto/clipboard"
 	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/session"
 	"github.com/gdamore/tcell/v2"
@@ -11,37 +8,16 @@ import (
 	"github.com/rivo/tview"
 )
 
-func NewMessageInputField(s *session.Session, c discord.Channel, theme *util.Theme) *tview.InputField {
+func NewMessageInputField(onMessageInputFieldInputCapture func(event *tcell.EventKey) *tcell.EventKey, s *session.Session, c discord.Channel, theme *util.Theme) *tview.InputField {
 	i := tview.NewInputField()
 	i.
 		SetPlaceholder("Message...").
-		SetFieldWidth(0).
+		SetPlaceholderTextColor(tcell.ColorWhite).
 		SetFieldBackgroundColor(tcell.GetColor(theme.InputFieldBackground)).
 		SetBackgroundColor(tcell.GetColor(theme.InputFieldBackground)).
 		SetBorder(true).
 		SetBorderPadding(0, 0, 1, 1).
-		SetInputCapture(onMessageInputFieldInputCapture(i, s, c))
+		SetInputCapture(onMessageInputFieldInputCapture)
 
 	return i
-}
-
-func onMessageInputFieldInputCapture(i *tview.InputField, s *session.Session, c discord.Channel) func(event *tcell.EventKey) *tcell.EventKey {
-	return func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		case tcell.KeyEnter:
-			t := strings.TrimSpace(i.GetText())
-			if t == "" {
-				return nil
-			}
-
-			i.SetText("")
-			s.SendMessage(c.ID, t)
-		case tcell.KeyCtrlV:
-			text, _ := clipboard.ReadAll()
-			text = i.GetText() + text
-			i.SetText(text)
-		}
-
-		return event
-	}
 }
