3fc408f8f0250f07a42df4f8cb9ac4435a97170e
rigormorrtiss <rigormorrtiss@gmail.com>
2021-08-15 09:55:36 +0400

    fix: increase guilds TreeView fixedSize

--- ui/flex.go
+++ ui/flex.go
@@ -10,7 +10,7 @@ func NewMainFlex(treeV *tview.TreeView, textV *tview.TextView, i *tview.InputFie
 		AddItem(textV, 0, 1, false).
 		AddItem(i, 3, 1, false)
 	mainFlex = tview.NewFlex().
-		AddItem(treeV, 25, 1, false).
+		AddItem(treeV, 30, 1, false).
 		AddItem(rightFlex, 0, 1, false)
 
 	return mainFlex
