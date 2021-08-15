6f8d9cca1c3f67adac77ced1ed63bb30da51e991
rigormorrtiss <rigormorrtiss@gmail.com>
2021-08-15 19:04:12 +0400

    feat(ui): modify message InputField fixedSize to 25

--- ui/flex.go
+++ ui/flex.go
@@ -10,7 +10,7 @@ func NewMainFlex(treeV *tview.TreeView, textV *tview.TextView, i *tview.InputFie
 		AddItem(textV, 0, 1, false).
 		AddItem(i, 3, 1, false)
 	mf = tview.NewFlex().
-		AddItem(treeV, 30, 1, false).
+		AddItem(treeV, 25, 1, false).
 		AddItem(rf, 0, 1, false)
 
 	return mf
