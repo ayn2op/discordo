29abdf8a21b3712cf77788e0cedf445c6684bcd8
rigormorrtiss <rigormorrtiss@gmail.com>
2021-08-17 18:58:46 +0400

    feat(ui): add ui.NewMfaLoginForm()

--- ui/forms.go
+++ ui/forms.go
@@ -5,16 +5,31 @@ import (
 	"github.com/rivo/tview"
 )
 
-func NewLoginForm(onLoginFormLoginButtonSelected func()) (f *tview.Form) {
+func newBaseLoginForm() (f *tview.Form) {
 	f = tview.NewForm()
 	f.
-		AddInputField("Email", "", 0, nil, nil).
-		AddPasswordField("Password", "", 0, 0, nil).
-		AddButton("Login", onLoginFormLoginButtonSelected).
 		SetButtonsAlign(tview.AlignCenter).
 		SetButtonBackgroundColor(tcell.GetColor("#5865F2")).
 		SetBorder(true).
 		SetBorderPadding(0, 0, 1, 0)
 
-	return f
+	return
+}
+
+func NewLoginForm(onLoginFormLoginButtonSelected func()) (f *tview.Form) {
+	f = newBaseLoginForm()
+	f.
+		AddInputField("Email", "", 0, nil, nil).
+		AddPasswordField("Password", "", 0, 0, nil).
+		AddButton("Login", onLoginFormLoginButtonSelected)
+
+	return
+}
+
+func NewMfaLoginForm(onMfaLoginFormLoginButtonSelected func()) (f *tview.Form) {
+	f = newBaseLoginForm().
+		AddPasswordField("Code", "", 0, 0, nil).
+		AddButton("Login", onMfaLoginFormLoginButtonSelected)
+
+	return
 }
