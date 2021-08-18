337c56bc68b15168a8f24249ad6ebdeddb529a72
rigormorrtiss <rigormorrtiss@gmail.com>
2021-08-18 20:22:51 +0400

    fix(ui): remove redundant \n from guilds list

--- ui/lists.go
+++ ui/lists.go
@@ -5,9 +5,11 @@ import (
 	"github.com/rivo/tview"
 )
 
-func NewGuildsList(t *util.Theme) (l *tview.List) {
+func NewGuildsList(onGuildsListSelected func(int, string, string, rune), t *util.Theme) (l *tview.List) {
 	l = tview.NewList()
 	l.
+		SetSelectedFunc(onGuildsListSelected).
+		ShowSecondaryText(false).
 		SetBorder(true).
 		SetBorderPadding(0, 0, 1, 0).
 		SetTitle("Guilds").
