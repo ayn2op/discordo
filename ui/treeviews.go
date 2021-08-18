bca4054100c4e8f1482348ba60bf4c1f5a0ab9fb
rigormorrtiss <rigormorrtiss@gmail.com>
2021-08-18 20:24:07 +0400

    refactor(ui): rename TreeView to channels TreeView

--- ui/treeviews.go
+++ ui/treeviews.go
@@ -4,15 +4,15 @@ import (
 	"github.com/rivo/tview"
 )
 
-func NewGuildsTreeView(onGuildsTreeViewSelected func(*tview.TreeNode)) (treeV *tview.TreeView) {
+func NewChannelsTreeView(onChannelsTreeViewSelected func(*tview.TreeNode)) (treeV *tview.TreeView) {
 	treeV = tview.NewTreeView()
 	treeN := tview.NewTreeNode("")
 	treeV.
 		SetTopLevel(1).
 		SetRoot(treeN).
 		SetCurrentNode(treeN).
-		SetSelectedFunc(onGuildsTreeViewSelected).
-		SetTitle("Guilds").
+		SetSelectedFunc(onChannelsTreeViewSelected).
+		SetTitle("Channels").
 		SetBorder(true).
 		SetBorderPadding(0, 0, 1, 0).
 		SetTitleAlign(tview.AlignLeft)
