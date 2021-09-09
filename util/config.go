ba336aebc5c85c6d055042593fffb7852a1fa845
rigormorrtiss <rigormorrtiss@gmail.com>
2021-09-09 19:59:13 +0400

    feat(util): add UserAgent field to Config (#41)
    * feat(util): add UserAgent to Config
    
    * feat(util): add UserAgent field to Config

--- util/config.go
+++ util/config.go
@@ -10,17 +10,24 @@ import (
 // Config consists of fields, such as theme, mouse, so on, that may be
 // customized by the user.
 type Config struct {
-	Token            string       `json:"token,omitempty"`
-	Mouse            bool         `json:"mouse,omitempty"`
-	GetMessagesLimit int          `json:"getMessagesLimit,omitempty"`
-	Theme            *tview.Theme `json:"theme,omitempty"`
+	Token            string      `json:"token"`
+	Mouse            bool        `json:"mouse"`
+	UserAgent        string      `json:"userAgent"`
+	GetMessagesLimit int         `json:"getMessagesLimit"`
+	Theme            tview.Theme `json:"theme"`
 }
 
 // NewConfig reads the configuration file (if exists) and returns a new config.
 func NewConfig() *Config {
 	c := Config{
-		Mouse:            true,
+		Token: "",
+		Mouse: true,
+		UserAgent: "" +
+			"Mozilla/5.0 (X11; Linux x86_64) " +
+			"AppleWebKit/537.36 (KHTML, like Gecko) " +
+			"Chrome/92.0.4515.131 Safari/537.36",
 		GetMessagesLimit: 50,
+		Theme:            tview.Styles,
 	}
 
 	userHomeDir, err := os.UserHomeDir()
