1aefedfaec84becfd5688ecfec71d993bf306192
ayntgl <ayntgl@gmail.com>
2021-10-15 23:26:46 +0400

    Move generateChannelRepr() to ui.go

--- ui.go
+++ ui.go
@@ -108,6 +108,25 @@ func onChannelsTreeSelected(n *tview.TreeNode) {
 	}
 }
 
+func generateChannelRepr(c *discordgo.Channel) string {
+	var repr string
+	if c.Name != "" {
+		repr = "#" + c.Name
+	} else if len(c.Recipients) == 1 {
+		rp := c.Recipients[0]
+		repr = rp.Username + "#" + rp.Discriminator
+	} else {
+		rps := make([]string, len(c.Recipients))
+		for i, r := range c.Recipients {
+			rps[i] = r.Username + "#" + r.Discriminator
+		}
+
+		repr = strings.Join(rps, ", ")
+	}
+
+	return repr
+}
+
 func createTopLevelChannelsTreeNodes(
 	n *tview.TreeNode,
 	cs []*discordgo.Channel,
