f6061df2981a8841411c95cda760cf2d5fe27f65
chromium <chromiumcatyt@gmail.com>
2021-10-25 19:19:28 +0200

    feat: copy message to clipboard using keybinding (#80)
    * copy message
    
    * default keybinding
    
    Co-authored-by: chromium <chromium@chromium.codes>

--- config.go
+++ config.go
@@ -23,6 +23,7 @@ type keybindings struct {
 	SelectLastMessage           string `toml:"select_last_message"`
 	ReplySelectedMessage        string `toml:"reply_selected_message"`
 	MentionReplySelectedMessage string `toml:"mention_reply_selected_message"`
+	CopySelectedMessage	    string `toml:"copy_selected_message"`
 }
 
 type theme struct {
@@ -107,6 +108,7 @@ func loadConfig() *config {
 			SelectLastMessage:           "End",
 			ReplySelectedMessage:        "Rune[r]",
 			MentionReplySelectedMessage: "Rune[R]",
+			CopySelectedMessage: "Rune[c]",
 		}
 		c.Borders = tview.Borders
 
