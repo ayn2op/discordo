2be06ef4cda65dfb401eeaf998473936b0334d1c
ayntgl <90600421+ayntgl@users.noreply.github.com>
2021-10-29 15:26:01 +0400

    feat: lazy-load channels on guild select (#89)
    * feat: set channels tree title to 'Channels'
    
    * feat: lazy-load channels on guild select

--- util.go
+++ util.go
@@ -1,69 +1,10 @@
 package main
 
 import (
-	"sort"
-
 	"github.com/ayntgl/discordgo"
 	"github.com/ayntgl/discordo/util"
 	"github.com/rivo/tview"
 )
-
-func createPrivateChannels(n *tview.TreeNode) {
-	cs := session.State.PrivateChannels
-	sort.Slice(cs, func(i, j int) bool {
-		return cs[i].LastMessageID > cs[j].LastMessageID
-	})
-
-	for _, c := range cs {
-		var tag string
-		if util.ChannelIsUnread(session.State, c) {
-			tag = "[::b]"
-		} else {
-			tag = "[::d]"
-		}
-
-		cn := tview.NewTreeNode(tag + util.ChannelToString(c) + "[::-]").
-			SetReference(c.ID)
-		n.AddChild(cn)
-	}
-}
-
-func createGuilds(n *tview.TreeNode) {
-	gs := session.State.Guilds
-	sort.Slice(gs, func(a, b int) bool {
-		found := false
-		for _, gID := range session.State.Settings.GuildPositions {
-			if found {
-				if gID == gs[b].ID {
-					return true
-				}
-			} else {
-				if gID == gs[a].ID {
-					found = true
-				}
-			}
-		}
-
-		return false
-	})
-
-	for _, g := range gs {
-		gn := tview.NewTreeNode(g.Name).Collapse()
-		n.AddChild(gn)
-
-		cs := g.Channels
-		sort.Slice(cs, func(i, j int) bool {
-			return cs[i].Position < cs[j].Position
-		})
-
-		// Top-level channels
-		createTopLevelChannelsTreeNodes(gn, cs)
-		// Category channels
-		createCategoryChannelsTreeNodes(gn, cs)
-		// Second-level channels
-		createSecondLevelChannelsTreeNodes(cs)
-	}
-}
 
 func createTopLevelChannelsTreeNodes(
 	n *tview.TreeNode,
