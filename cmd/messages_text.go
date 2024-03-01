bdb2e16a06f697a2761fee148deb9fb7fe43af31
cyberme0w <me@cyberme0w.dev>
2024-03-01 12:39:02 +0100

    Append original message's datetime (time only if same-day) to reply message input title

--- cmd/messages_text.go
+++ cmd/messages_text.go
@@ -185,7 +185,20 @@ func (mt *MessagesText) replyAction(mention bool) {
 		return
 	}
 
+	// Add username to reply title
 	title += ms[mt.selectedMessage].Author.Tag()
+
+	// Add timestamp to reply title
+	var timestamp string
+	sy, sm, sd := ms[mt.selectedMessage].Timestamp.Time().Date()
+	ny, nm, nd := time.Now().Date()
+	if sy == ny && sm == nm && sd == nd {
+		timestamp = ms[mt.selectedMessage].Timestamp.Time().Format("15:04:05")
+	} else {
+		timestamp = ms[mt.selectedMessage].Timestamp.Time().Format("2006-01-02 15:04:05")
+	}
+
+	title += fmt.Sprintf(" @ %s", timestamp)
 	mainFlex.messageInput.SetTitle(title)
 	mainFlex.messageInput.replyMessageIdx = mt.selectedMessage
 
