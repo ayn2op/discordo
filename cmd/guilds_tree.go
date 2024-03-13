7eec0d6be64215211668d668d81be776f376c09f
cyberme0w <me@cyberme0w.dev>
2024-03-13 09:54:11 +0100

    Add UserList to MainFlex
    This involves some changes:
    - Add booleans to store UI element visibility, instead of counting
      children (guildsTreeVisible/userListVisible)
    - rename 'right' to 'chat', as it no longer is the right-most UI element
    - Adding cases to onInputCapture

--- cmd/guilds_tree.go
+++ cmd/guilds_tree.go
@@ -199,6 +199,8 @@ func (gt *GuildsTree) onSelected(n *tview.TreeNode) {
 
 		gt.selectedChannelID = ref
 		app.SetFocus(mainFlex.messageInput)
+
+		mainFlex.userList.updateUsersFromGuildID(c.GuildID)
 	case nil: // Direct messages
 		cs, err := discordState.Cabinet.PrivateChannels()
 		if err != nil {
