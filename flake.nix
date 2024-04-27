2a4f866c35ac0419bdefca947b967d97296a4476
emanueljg <emanueljohnsongodin@gmail.com>
2024-04-27 18:08:01 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,38 @@
-{ 
+{
   description = "A lightweight, secure, and feature-rich Discord terminal client.";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, ... }: let 
+  outputs = { self, nixpkgs, ... }:
+    let
 
-    supportedSystems = [ 
-      "x86_64-linux" 
-      "aarch64-linux"
-      "x86_64-darwin"
-    ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-    forSupportedSystems = f:
-      builtins.listToAttrs (builtins.map 
-        (system: 
-          { name = system; value = f system (import nixpkgs { inherit system; }); 
-        }) 
-        supportedSystems);
-    
-  in {
-    packages = forSupportedSystems (system: pkgs: 
-      let pkg = pkgs.buildGoModule {
-        pname = "discordo";
-        version = builtins.substring 0 8 self.lastModifiedDate;
-        src = ./.;
-        vendorSha256 = "sha256-5Y+SP374Bd8F2ABKEKRhTcGNhsFM77N5oC5wRN6AzKk=";
-      };
-    in {
-      default = pkg;
-      discordo = pkg;
-    });
-  };
+      forSupportedSystems = f:
+        builtins.listToAttrs (builtins.map
+          (system:
+            {
+              name = system;
+              value = f system (import nixpkgs { inherit system; });
+            })
+          supportedSystems);
+
+    in
+    {
+      packages = forSupportedSystems (system: pkgs: {
+        default = pkgs.buildGo122Module {
+          pname = "discordo";
+          version = builtins.substring 0 8 self.lastModifiedDate;
+          src = ./.;
+          vendorHash = "sha256-hSrGN3NHPpp5601l4KcmNHVYOGWfLjFeWWr9g11nM3I=";
+        };
+      });
+    };
 }
   
 
