754168faa48c750f7da75468bb882e9b2451784d
ayn2op <ayn2op@gmail.com>
2024-12-20 18:15:47 -0500

    refactor: move token retrieval login to run.go

--- cmd/run.go
+++ cmd/run.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"log/slog"
+
 	"github.com/ayn2op/discordo/internal/config"
 	"github.com/ayn2op/discordo/internal/logger"
+	"github.com/zalando/go-keyring"
 )
 
 var (
@@ -13,6 +16,16 @@ var (
 func Run(token string) error {
 	if err := logger.Load(); err != nil {
 		return err
+	}
+
+	// If no token was provided, look it up in the keyring.
+	if token == "" {
+		tok, err := keyring.Get(config.Name, "token")
+		if err != nil {
+			slog.Info("failed to get token from keyring", "err", err)
+		} else {
+			token = tok
+		}
 	}
 
 	cfg, err := config.Load()
