e386077b96cb8e4d472fd5db7a324a9c317b5731
ayn2op <ayn2op@gmail.com>
2024-12-20 18:15:47 -0500

    refactor: move token retrieval login to run.go

--- main.go
+++ main.go
@@ -5,23 +5,11 @@ import (
 	"log/slog"
 
 	"github.com/ayn2op/discordo/cmd"
-	"github.com/ayn2op/discordo/internal/config"
-	"github.com/zalando/go-keyring"
 )
 
 func main() {
 	token := flag.String("token", "", "authentication token")
 	flag.Parse()
-
-	// If no token was provided, look it up in the keyring
-	if *token == "" {
-		t, err := keyring.Get(config.Name, "token")
-		if err != nil {
-			slog.Info("failed to get token from keyring", "err", err)
-		} else {
-			*token = t
-		}
-	}
 
 	if err := cmd.Run(*token); err != nil {
 		slog.Error("failed to run", "err", err)
