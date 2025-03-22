1628535b5d5b9f378f886ce7d0c977ff0558af47
Ayyan <ayn2op@gmail.com>
2025-03-22 01:29:17 -0400

    refactor: use cobra (#515)

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"flag"
 	"log/slog"
 
 	"github.com/ayn2op/discordo/cmd"
 )
 
 func main() {
-	token := flag.String("token", "", "authentication token")
-	flag.Parse()
-
-	if err := cmd.Run(*token); err != nil {
-		slog.Error(err.Error())
+	if err := cmd.Execute(); err != nil {
+		slog.Error("failed to execute command", "err", err)
 	}
 }
