c9eb6b54f64b73dafcb57e3ad3b3d1d162270cba
ayn2op <ayn2op@gmail.com>
2025-03-30 23:53:42 -0400

    refactor(ui): add NewConfiguredBox func

--- internal/ui/util.go
+++ internal/ui/util.go
@@ -1,6 +1,28 @@
 package ui
 
-import "github.com/rivo/tview"
+import (
+	"github.com/ayn2op/discordo/internal/config"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+func NewConfiguredBox(box *tview.Box, cfg *config.Theme) *tview.Box {
+	b := cfg.Border
+	p := b.Padding
+	box.
+		SetBorder(cfg.Border.Enabled).
+		SetBorderPadding(p[0], p[1], p[2], p[3]).
+		SetTitleAlign(tview.AlignLeft).
+		SetFocusFunc(func() {
+			box.SetBorderColor(tcell.GetColor(b.ActiveColor))
+			box.SetTitleColor(tcell.GetColor(cfg.ActiveTitleColor))
+		}).
+		SetBlurFunc(func() {
+			box.SetBorderColor(tcell.GetColor(b.Color))
+			box.SetTitleColor(tcell.GetColor(cfg.TitleColor))
+		})
+	return box
+}
 
 func Centered(p tview.Primitive, width, height int) tview.Primitive {
 	return tview.NewGrid().
