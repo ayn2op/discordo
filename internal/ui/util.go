eaa31de23d5084a950d99d06c2bf801f1ed0bfbf
ayn2op <ayn2op@gmail.com>
2025-06-03 20:59:10 -0400

    refactor: use SetTitleStyle instead

--- internal/ui/util.go
+++ internal/ui/util.go
@@ -11,16 +11,21 @@ func NewConfiguredBox(box *tview.Box, cfg *config.Theme) *tview.Box {
 	t := cfg.Title
 	p := b.Padding
 	box.
-		SetBorderColor(tcell.GetColor(b.Color)).
 		SetBorderPadding(p[0], p[1], p[2], p[3]).
 		SetTitleAlign(int(t.Align)).
 		SetFocusFunc(func() {
-			box.SetBorderColor(tcell.GetColor(b.ActiveColor))
-			box.SetTitleColor(tcell.GetColor(t.ActiveColor))
+			borderColor := tcell.GetColor(b.ActiveColor)
+			box.SetBorderStyle(tcell.StyleDefault.Foreground(borderColor))
+
+			titleColor := tcell.GetColor(t.ActiveColor)
+			box.SetTitleStyle(tcell.StyleDefault.Foreground(titleColor))
 		}).
 		SetBlurFunc(func() {
-			box.SetBorderColor(tcell.GetColor(b.Color))
-			box.SetTitleColor(tcell.GetColor(t.Color))
+			borderColor := tcell.GetColor(b.Color)
+			box.SetBorderStyle(tcell.StyleDefault.Foreground(borderColor))
+
+			titleColor := tcell.GetColor(t.Color)
+			box.SetTitleStyle(tcell.StyleDefault.Foreground(titleColor))
 		})
 
 	if b.Enabled {
