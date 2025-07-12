7fe4ff7df1c5a315f442d35be6a50a7520b1967b
ayn2op <ayn2op@gmail.com>
2025-07-12 15:10:59 -0400

    config: refactor

--- internal/config/theme.go
+++ internal/config/theme.go
@@ -7,31 +7,7 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
-var (
-	errInvalidType = errors.New("invalid type")
-)
-
-type BorderSetWrapper struct{ tview.BorderSet }
-
-func (bw *BorderSetWrapper) UnmarshalTOML(val any) error {
-	s, ok := val.(string)
-	if !ok {
-		return errInvalidType
-	}
-
-	switch s {
-	case "plain":
-		bw.BorderSet = tview.BorderSetPlain()
-	case "round":
-		bw.BorderSet = tview.BorderSetRound()
-	case "thick":
-		bw.BorderSet = tview.BorderSetThick()
-	case "double":
-		bw.BorderSet = tview.BorderSetDouble()
-	}
-
-	return nil
-}
+var errInvalidType = errors.New("invalid type")
 
 type AlignmentWrapper struct{ tview.Alignment }
 
@@ -111,18 +87,6 @@ type (
 	ThemeStyle struct {
 		NormalStyle StyleWrapper `toml:"normal_style"`
 		ActiveStyle StyleWrapper `toml:"active_style"`
-	}
-
-	TitleTheme struct {
-		ThemeStyle
-		Alignment AlignmentWrapper `toml:"alignment"`
-	}
-
-	BorderTheme struct {
-		ThemeStyle
-		Enabled bool             `toml:"enabled"`
-		Padding [4]int           `toml:"padding"`
-		Set     BorderSetWrapper `toml:"set"`
 	}
 
 	GuildsTreeTheme struct {
