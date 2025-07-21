19bcf6cb4a113c40c493943a5918a854708142ed
ayn2op <ayn2op@gmail.com>
2025-07-20 21:25:55 -0400

    guildstree: implement tea.Model

---
+++ guildstree/model.go
@@ -0,0 +1,22 @@
+package guildstree
+
+import tea "github.com/charmbracelet/bubbletea"
+
+type Model struct {
+}
+
+func NewModel() Model {
+	return Model{}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m Model) View() string {
+	return "tree"
+}
