eb3da91603e8cb73e4135f6fd5164e601ee018ed
Abdullah <3bd3bdr@gmail.com>
2025-11-13 22:13:58 +0300

    rename(config): Rename x_indentation into x_indent

--- internal/config/theme.go
+++ internal/config/theme.go
@@ -136,11 +136,11 @@ type (
 		AutoExpandFolders bool   `toml:"auto_expand_folders"`
 		Graphics          bool   `toml:"graphics"`
 		GraphicsColor     string `toml:"graphics_color"`
-		FolderItemsIndent int   `toml:"folder_items_indentation"`
-		CategoryIndent    int   `toml:"category_indentation"`
-		ChannelIndent     int   `toml:"channel_indentation"`
-		GroupDMIndent     int   `toml:"groupdm_indentation"`
-		DMIndent          int   `toml:"dm_indentation"`
+		FolderItemsIndent int   `toml:"folder_items_indent"`
+		CategoryIndent    int   `toml:"category_indent"`
+		ChannelIndent     int   `toml:"channel_indent"`
+		GroupDMIndent     int   `toml:"groupdm_indent"`
+		DMIndent          int   `toml:"dm_indent"`
 	}
 
 	MessagesListTheme struct {
