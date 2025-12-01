0b5a7da23504efd044fca899a9858a4eda1ca08f
harpyfox <harper.iro@gmail.com>
2025-12-01 18:46:21 +1100

    feat(statusbar): use config keybinds! and style updates

--- cmd/status_bar.go
+++ cmd/status_bar.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/ayn2op/discordo/internal/config"
 	"github.com/ayn2op/discordo/internal/ui"
 	"github.com/ayn2op/tview"
@@ -24,6 +27,7 @@ func newStatusBar(cfg *config.Config) *statusBar {
 		SetBlurFunc(nil).
 		SetFocusFunc(nil)
 	sb.
+		SetDynamicColors(true).
 		SetWrap(false).
 		SetScrollable(false).
 		SetTitle("")
@@ -31,40 +35,85 @@ func newStatusBar(cfg *config.Config) *statusBar {
 	return sb
 }
 
-// TODO)) ideally these are NOT hardcoded rofl
 func (sb *statusBar) update(app *application) {
 	if app.chatView != nil {
-		helpString := ""
+		var keybinds []string
+		cfg := app.cfg
+
 		var f = app.GetFocus()
 		switch f {
 		case app.chatView.guildsTree:
-			helpString += "k prev j next g first G last RTN select"
+			keybinds = append(keybinds,
+				fmtKeybind(cfg.Keys.GuildsTree.SelectPrevious, "prev"),
+				fmtKeybind(cfg.Keys.GuildsTree.SelectNext, "next"),
+				fmtKeybind(cfg.Keys.GuildsTree.SelectFirst, "first"),
+				fmtKeybind(cfg.Keys.GuildsTree.SelectLast, "last"),
+			)
 			// TODO)) expand/collapse
+			// cfg.Keys.GuildsTree.SelectCurrent, "select"
+			// cfg.Keys.GuildsTree.SelectCurrent, "expand"
+			// cfg.Keys.GuildsTree.SelectCurrent, "collapse"
 		case app.chatView.messagesList:
-			helpString += "k prev j next g first G last"
+			keybinds = append(keybinds,
+				fmtKeybind(cfg.Keys.MessagesList.SelectPrevious, "prev"),
+				fmtKeybind(cfg.Keys.MessagesList.SelectNext, "next"),
+				fmtKeybind(cfg.Keys.MessagesList.SelectFirst, "first"),
+				fmtKeybind(cfg.Keys.MessagesList.SelectLast, "last"),
+			)
 			msg, err := app.chatView.messagesList.selectedMessage()
 			if err != nil {
 				// noop i think?
 			} else {
-				helpString += " r reply R @reply"
+				keybinds = append(keybinds,
+					fmtKeybind(cfg.Keys.MessagesList.Reply, "reply"),
+					fmtKeybind(cfg.Keys.MessagesList.ReplyMention, "@reply"),
+				)
 				urls := ui.ExtractURLs(msg.Content)
 				if len(urls)+len(msg.Attachments) > 0 {
-					helpString += " o open"
+					keybinds = append(keybinds,
+						fmtKeybind(cfg.Keys.MessagesList.Open, "open"),
+					)
 				}
 				if ref := msg.ReferencedMessage; ref != nil {
-					helpString += " s goto OP"
+					keybinds = append(keybinds,
+						fmtKeybind(cfg.Keys.MessagesList.SelectReply, "goto OP"),
+					)
 				}
-				// TODO)) edit/delete own messages, attachments list
+				// TODO)) edit/delete own messages
+				// cfg.Keys.MessagesList.Edit, "edit"
+				// cfg.Keys.MessagesList.Delete, "delete"
+				// cfg.Keys.MessagesList.DeleteConfirm, "confirm delete"
+				// TODO)) attachments list
 			}
 		case app.chatView.messageInput:
 			// TODO))
-			helpString += "RTN send ALT-RTN newline ESC clear CTRL-\\ attach"
+			keybinds = append(keybinds,
+				fmtKeybind(cfg.Keys.MessageInput.Send, "send"),
+				fmtKeybind(cfg.Keys.MessageInput.Cancel, "clear"),
+				fmtKeybind(cfg.Keys.MessageInput.OpenFilePicker, "attach"),
+			)
 		default:
 			// TODO)) mouse input seems to cause this case, not sure of a solution :(
-			helpString += " (mouse controls dont play nice with status bar atm.)"
+			keybinds = append(keybinds,
+				"(status bar not playing nice with mouse input atm.)",
+			)
 		}
-		sb.setText(helpString)
+		// TODO)) expose separator to theme?
+		sb.setText(strings.Join(keybinds, "  "))
 	}
+}
+
+func fmtKeybind(keybind string, name string) string {
+	// this is seriously ugly and probably not performance friendly
+	key := keybind
+	if strings.HasPrefix(key, "Rune[") {
+		key = fmt.Sprintf("%c", []rune(keybind)[5]) // bro
+	}
+	key = strings.Replace(key, "Ctrl+", "^", 1)
+	key = strings.Replace(key, "Enter", "CR", 1)
+	key = strings.Replace(key, "Esc", "ESC", 1)
+	// TODO)) expose format to theme?
+	return fmt.Sprintf("[::r][::b] %v [::B][::R] %v", key, name)
 }
 
 func (sb *statusBar) setText(t string) {
