74531d1ece68be5c588f48f44e561750e4025a8e
harpyfox <harper.iro@gmail.com>
2025-12-01 21:49:33 +1100

    feat(statusbar): mooooore recognised keybinds

--- cmd/status_bar.go
+++ cmd/status_bar.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ayn2op/discordo/internal/config"
 	"github.com/ayn2op/discordo/internal/ui"
 	"github.com/ayn2op/tview"
+	"github.com/diamondburned/arikawa/v3/discord"
 )
 
 type statusBar struct {
@@ -35,6 +36,14 @@ func newStatusBar(cfg *config.Config) *statusBar {
 	return sb
 }
 
+func fmtNavigationKeybinds(navigationKeys config.NavigationKeys) []string {
+	return []string{
+		fmtKeybind(navigationKeys.SelectPrevious, "prev"),
+		fmtKeybind(navigationKeys.SelectNext, "next"),
+		fmtKeybind(navigationKeys.SelectFirst, "first"),
+		fmtKeybind(navigationKeys.SelectLast, "last")}
+}
+
 func (sb *statusBar) update(app *application) {
 	if app.chatView != nil {
 		var keybinds []string
@@ -43,33 +52,36 @@ func (sb *statusBar) update(app *application) {
 		var f = app.GetFocus()
 		switch f {
 		case app.chatView.guildsTree:
-			keybinds = append(keybinds,
-				fmtKeybind(cfg.Keys.GuildsTree.SelectPrevious, "prev"),
-				fmtKeybind(cfg.Keys.GuildsTree.SelectNext, "next"),
-				fmtKeybind(cfg.Keys.GuildsTree.SelectFirst, "first"),
-				fmtKeybind(cfg.Keys.GuildsTree.SelectLast, "last"),
-			)
-			// TODO)) expand/collapse
-			// cfg.Keys.GuildsTree.SelectCurrent, "select"
-			// cfg.Keys.GuildsTree.SelectCurrent, "expand"
-			// cfg.Keys.GuildsTree.SelectCurrent, "collapse"
+			keybinds = append(keybinds, fmtNavigationKeybinds(cfg.Keys.GuildsTree.NavigationKeys)...)
+			node := app.chatView.guildsTree.GetCurrentNode()
+
+			if node != nil {
+				switch node.GetReference().(type) {
+				case discord.ChannelID:
+					keybinds = append(keybinds,
+						fmtKeybind(cfg.Keys.GuildsTree.SelectCurrent, "select"),
+					)
+				default:
+					if len(node.GetChildren()) == 0 || !node.IsExpanded() {
+						keybinds = append(keybinds,
+							fmtKeybind(cfg.Keys.GuildsTree.SelectCurrent, "expand"),
+						)
+					} else {
+						keybinds = append(keybinds,
+							fmtKeybind(cfg.Keys.GuildsTree.SelectCurrent, "collapse"),
+						)
+					}
+				}
+			}
+
 		case app.chatView.messagesList:
-			keybinds = append(keybinds,
-				fmtKeybind(cfg.Keys.MessagesList.SelectPrevious, "prev"),
-				fmtKeybind(cfg.Keys.MessagesList.SelectNext, "next"),
-				fmtKeybind(cfg.Keys.MessagesList.SelectFirst, "first"),
-				fmtKeybind(cfg.Keys.MessagesList.SelectLast, "last"),
-			)
-			msg, err := app.chatView.messagesList.selectedMessage()
-			if err != nil {
-				// noop i think?
-			} else {
+			keybinds = append(keybinds, fmtNavigationKeybinds(cfg.Keys.MessagesList.NavigationKeys)...)
+			if msg, err := app.chatView.messagesList.selectedMessage(); err == nil {
 				keybinds = append(keybinds,
 					fmtKeybind(cfg.Keys.MessagesList.Reply, "reply"),
 					fmtKeybind(cfg.Keys.MessagesList.ReplyMention, "@reply"),
 				)
-				urls := ui.ExtractURLs(msg.Content)
-				if len(urls)+len(msg.Attachments) > 0 {
+				if len(ui.ExtractURLs(msg.Content))+len(msg.Attachments) > 0 {
 					keybinds = append(keybinds,
 						fmtKeybind(cfg.Keys.MessagesList.Open, "open"),
 					)
@@ -79,41 +91,63 @@ func (sb *statusBar) update(app *application) {
 						fmtKeybind(cfg.Keys.MessagesList.SelectReply, "goto OP"),
 					)
 				}
-				// TODO)) edit/delete own messages
-				// cfg.Keys.MessagesList.Edit, "edit"
-				// cfg.Keys.MessagesList.Delete, "delete"
-				// cfg.Keys.MessagesList.DeleteConfirm, "confirm delete"
-				// TODO)) attachments list
+				me, err := discordState.Cabinet.Me()
+				if err == nil && me.ID == msg.Author.ID {
+					keybinds = append(keybinds,
+						fmtKeybind(cfg.Keys.MessagesList.Edit, "edit"),
+						fmtKeybind(cfg.Keys.MessagesList.DeleteConfirm, "delete"),
+					)
+				}
 			}
+
 		case app.chatView.messageInput:
-			// TODO))
-			keybinds = append(keybinds,
-				fmtKeybind(cfg.Keys.MessageInput.Send, "send"),
-				fmtKeybind(cfg.Keys.MessageInput.Cancel, "clear"),
-				fmtKeybind(cfg.Keys.MessageInput.OpenFilePicker, "attach"),
-			)
+			if app.chatView.GetVisibile(mentionsListPageName) {
+				keybinds = append(keybinds,
+					fmtKeybind(cfg.Keys.MessageInput.TabComplete, "accept"),
+					fmtKeybind(cfg.Keys.MessageInput.Cancel, "dismiss"),
+					fmtKeybind(cfg.Keys.MentionsList.Up, "up"),
+					fmtKeybind(cfg.Keys.MentionsList.Down, "down"),
+				)
+			} else {
+				keybinds = append(keybinds,
+					fmtKeybind(cfg.Keys.MessageInput.Send, "send"),
+					fmtKeybind(cfg.Keys.MessageInput.Cancel, "clear"),
+					fmtKeybind(cfg.Keys.MessageInput.OpenFilePicker, "attach"),
+				)
+			}
+
 		default:
-			// TODO)) mouse input seems to cause this case, not sure of a solution :(
-			keybinds = append(keybinds,
-				"(status bar not playing nice with mouse input atm.)",
-			)
+			if app.chatView.GetPageCount() > 1 {
+				keybinds = append(keybinds,
+					fmtKeybind("↑→↓←", "navigate"),
+					fmtKeybind("Enter", "confirm"),
+					fmtKeybind("Esc", "cancel"),
+				)
+			} else {
+				// TODO)) mouse input seems to cause this case, not sure of a solution :(
+				keybinds = append(keybinds,
+					"(status bar not playing nice with mouse input atm.)",
+				)
+			}
 		}
-		// TODO)) expose separator to theme?
+
 		sb.setText(strings.Join(keybinds, "  "))
 	}
 }
 
-func fmtKeybind(keybind string, name string) string {
-	// this is seriously ugly and probably not performance friendly
+// this is seriously ugly and probably not performance friendly
+func fmtKeybind(keybind string, label string) string {
 	key := keybind
 	if strings.HasPrefix(key, "Rune[") {
 		key = fmt.Sprintf("%c", []rune(keybind)[5]) // bro
+	} else {
+		key = strings.Replace(key, "Ctrl+", "^", 1)
+		key = strings.Replace(key, "Alt+", "ALT-", 1)
+		key = strings.Replace(key, "Enter", "CR", 1)
+		key = strings.ToUpper(key)
 	}
-	key = strings.Replace(key, "Ctrl+", "^", 1)
-	key = strings.Replace(key, "Enter", "CR", 1)
-	key = strings.Replace(key, "Esc", "ESC", 1)
-	// TODO)) expose format to theme?
-	return fmt.Sprintf("[::r][::b] %v [::B][::R] %v", key, name)
+
+	return fmt.Sprintf("[::r][::b] %v [::B][::R] %v", key, label)
 }
 
 func (sb *statusBar) setText(t string) {
