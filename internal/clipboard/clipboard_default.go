30c5b7c36a04e3a5eb7168dabafa91d328cadf5f
Abdullah <3bd3bdr@gmail.com>
2025-12-10 03:45:10 +0300

    format(clipboard_default): Use "var"-style function forwarding

--- internal/clipboard/clipboard_default.go
+++ internal/clipboard/clipboard_default.go
@@ -4,14 +4,8 @@ package clipboard
 
 import designClipb "golang.design/x/clipboard"
 
-func Init() error {
-	return designClipb.Init()
-}
-
-func Read(t Format) []byte {
-	return designClipb.Read(designClipb.Format(t))
-}
-
-func Write(t Format, buf []byte) <-chan struct{} {
-	return designClipb.Write(designClipb.Format(t), buf)
-}
+var (
+	Init = designClipb.Init
+	Read = designClipb.Read
+	Write = designClipb.Write
+)
