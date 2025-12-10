2ecadef9c7fe89211fbd68b287237740e3315d6d
Abdullah <3bd3bdr@gmail.com>
2025-12-10 04:11:10 +0300

    format(clipboard): Convert redundant code into a function

--- internal/clipboard/clipboard_linux.go
+++ internal/clipboard/clipboard_linux.go
@@ -34,14 +34,9 @@ func Read(t Format) []byte {
 	if !wayland {
 		return designClipb.Read(designClipb.Format(t))
 	}
-	var typ string
-	switch t {
-	case FmtText:
-		typ = "text/plain;charset=utf-8"
-	case FmtImage:
-		typ = "image"
-	}
-	cmd := exec.Command("wl-paste", "--no-newline", "--type", typ)
+	// -n: Don't print a newline at the end
+	// -t type: MIME type specifier
+	cmd := exec.Command("wl-paste", "-nt", formatToType(t))
 	outBuffer := bytes.Buffer{}
 	cmd.Stdout = &outBuffer
 	if err := cmd.Run(); err != nil {
@@ -55,17 +50,22 @@ func Write(t Format, buf []byte) <-chan struct{} {
 	if !wayland {
 		return designClipb.Write(designClipb.Format(t), buf)
 	}
-	var typ string
-	switch t {
-	case FmtText:
-		typ = "text/plain;charset=utf-8"
-	case FmtImage:
-		typ = "image"
-	}
-	cmd := exec.Command("wl-copy", "--type", typ)
+	// -t type: MIME type specifier
+	cmd := exec.Command("wl-copy", "-t", formatToType(t))
 	cmd.Stdin = bytes.NewReader(buf)
 	if err := cmd.Run(); err != nil {
 		slog.Error("failed to write to clipboard", "err", err)
 	}
 	return nil
+}
+
+func formatToType(t Format) string {
+	switch t {
+	case FmtImage:
+		return "image"
+	case FmtText:
+		fallthrough
+	default:
+		return "text/plain;charset=utf-8"
+	}
 }
