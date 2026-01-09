bfdc29b8ebf1fe999f6f72927304e9d2d0d996b2
ayn2op <ayn2op@gmail.com>
2026-01-08 22:22:14 -0500

    remove existing code

--- main.go
+++ main.go
@@ -1,13 +1,3 @@
 package main
 
-import (
-	"log/slog"
-
-	"github.com/ayn2op/discordo/cmd"
-)
-
-func main() {
-	if err := cmd.Run(); err != nil {
-		slog.Error("failed to run command", "err", err)
-	}
-}
+func main() {}
