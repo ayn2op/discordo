8eea1bc2011cc9811d48137b13678e47a7de4546
ayn2op <ayn2op@gmail.com>
2026-01-08 22:26:29 -0500

    add root model

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"log/slog"
+	"os"
+
+	tea "charm.land/bubbletea/v2"
+	"github.com/ayn2op/discordo/internal/root"
+)
+
+func main() {
+	p := tea.NewProgram(root.NewModel())
+	if _, err := p.Run(); err != nil {
+		slog.Error("failed to run program", "err", err)
+		os.Exit(1)
+	}
+}
