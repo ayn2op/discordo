d200ad47edf6dd2ab167bffdecaf8f08bfe0fe26
ayn2op <ayn2op@gmail.com>
2026-01-10 22:23:57 -0500

    add flag support

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,28 @@
 package cmd
 
 import (
+	"flag"
+
 	"github.com/ayn2op/discordo/internal/config"
+	"github.com/ayn2op/discordo/internal/logger"
 	"github.com/ayn2op/discordo/internal/root"
 
 	tea "charm.land/bubbletea/v2"
 )
 
 func Run() error {
+	var (
+		token      string
+		configPath string
+		logPath    string
+		logLevel   string
+	)
+	flag.StringVar(&token, "token", "", "authentication token (default: $DISCORDO_TOKEN or keyring)")
+	flag.StringVar(&configPath, "config-path", config.DefaultPath(), "path of the configuration file")
+	flag.StringVar(&logPath, "log-path", logger.DefaultPath(), "path of the log file")
+	flag.StringVar(&logLevel, "log-level", "info", "log level")
+	flag.Parse()
+
 	cfg, err := config.Load(config.DefaultPath())
 	if err != nil {
 		return err
---
+++ internal/logger/logger.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+
+	"github.com/ayn2op/discordo/internal/consts"
+)
+
+const fileName = "logs.txt"
+
+func DefaultPath() string {
+	return filepath.Join(consts.CacheDir(), fileName)
+}
+
+// Load opens the log file and configures default logger.
+func Load(path string, level slog.Level) error {
+	if err := os.MkdirAll(filepath.Dir(path), os.ModePerm); err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to open log file: %w", err)
+	}
+
+	opts := &slog.HandlerOptions{Level: level}
+	handler := slog.NewTextHandler(file, opts)
+	slog.SetDefault(slog.New(handler))
+	return nil
+}
