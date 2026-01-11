7fbd174200b2bd4c350987b7e6313821d863772c
ayn2op <ayn2op@gmail.com>
2026-01-11 01:50:18 -0500

    dispatch TokenMsg on token form submit

--- internal/login/token.go
+++ internal/login/token.go
@@ -4,7 +4,19 @@ import (
 	"charm.land/bubbles/v2/textinput"
 	tea "charm.land/bubbletea/v2"
 	"github.com/ayn2op/discordo/form"
+	"github.com/ayn2op/discordo/internal/keyring"
 )
+
+type TokenMsg struct {
+	Value string
+	Err   error
+}
+
+func tokenMsgCmd(value string, err error) tea.Cmd {
+	return func() tea.Msg {
+		return TokenMsg{Value: value, Err: err}
+	}
+}
 
 type TokenModel struct {
 	form *form.Model
@@ -14,7 +26,6 @@ func newTokenModel() TokenModel {
 	input := textinput.New()
 	input.Placeholder = "Token"
 	input.EchoMode = textinput.EchoPassword
-
 	return TokenModel{
 		form: form.NewModel([]textinput.Model{input}),
 	}
@@ -28,7 +39,11 @@ func (m TokenModel) Init() tea.Cmd {
 
 func (m TokenModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	if m.form.Submitted {
-		panic("unimplemented")
+		token := m.form.Get(0).Value()
+		if err := keyring.SetToken(token); err != nil {
+			return m, tokenMsgCmd("", err)
+		}
+		return m, tokenMsgCmd(token, nil)
 	}
 
 	var cmd tea.Cmd
