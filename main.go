23796ce0fc9ab498ae91e0a629cd714fe0e13e82
ayn2op <ayn2op@gmail.com>
2026-01-31 12:38:58 -0500

    use log.Fatal instead to exit with error code 1

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"log/slog"
-	"os"
+	"log"
 
 	"github.com/ayn2op/discordo/cmd"
 )
 
 func main() {
 	if err := cmd.Run(); err != nil {
-		slog.Error("failed to run command", "err", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
