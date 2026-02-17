19dde46e1cbcffb04b50a8e2e4a6a837dade9e50
Abby K <abigail@minneelyyyy.dev>
2026-02-16 20:22:10 -0500

    avoid using $@

--- internal/config/editor_unix.go
+++ internal/config/editor_unix.go
@@ -4,8 +4,10 @@ package config
 
 import (
 	"os/exec"
+	"fmt"
 )
 
 func (cfg *Config) createEditorCommand(path string) *exec.Cmd {
-	return exec.Command("sh", "-c", cfg.Editor+" \"$@\"", cfg.Editor, path)
+	// return exec.Command("sh", "-c", cfg.Editor+" \"$@\"", cfg.Editor, path)
+	return exec.Command("sh", "-c", fmt.Sprintf("%s '%s'", cfg.Editor, path))
 }
