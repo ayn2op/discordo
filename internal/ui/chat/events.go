6e3f340b554f500a72c1459fde6d3ea2f3dbec54
ayn2op <ayn2op@gmail.com>
2026-03-19 19:15:14 -0400

    refactor(ui/chat): unify gateway and ui updates under event commands (#775)

--- internal/ui/chat/events.go
+++ internal/ui/chat/events.go
@@ -1,21 +1,24 @@
 package chat
 
 import (
+	"context"
 	"log/slog"
 
 	"github.com/ayn2op/tview"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/gdamore/tcell/v3"
 )
 
-type LogoutEvent struct{ tcell.EventTime }
-
-func (m *Model) logout() tview.Command {
+func (m *Model) openState() tview.Command {
 	return func() tview.Event {
-		return &LogoutEvent{}
+		if err := m.state.Open(context.Background()); err != nil {
+			slog.Error("failed to open chat state", "err", err)
+			return tcell.NewEventError(err)
+		}
+		return nil
 	}
 }
-
-type QuitEvent struct{ tcell.EventTime }
 
 func (m *Model) closeState() tview.Command {
 	return func() tview.Event {
@@ -28,6 +31,47 @@ func (m *Model) closeState() tview.Command {
 		return nil
 	}
 }
+
+type gatewayEvent struct {
+	tcell.EventTime
+	gateway.Event
+}
+
+func (m *Model) listen() tview.Command {
+	return func() tview.Event {
+		return &gatewayEvent{Event: <-m.events}
+	}
+}
+
+type channelLoadedEvent struct {
+	tcell.EventTime
+	Channel  discord.Channel
+	Messages []discord.Message
+}
+
+func newChannelLoadedEvent(channel discord.Channel, messages []discord.Message) *channelLoadedEvent {
+	return &channelLoadedEvent{Channel: channel, Messages: messages}
+}
+
+type olderMessagesLoadedEvent struct {
+	tcell.EventTime
+	ChannelID discord.ChannelID
+	Older     []discord.Message
+}
+
+func newOlderMessagesLoadedEvent(channelID discord.ChannelID, older []discord.Message) *olderMessagesLoadedEvent {
+	return &olderMessagesLoadedEvent{ChannelID: channelID, Older: older}
+}
+
+type LogoutEvent struct{ tcell.EventTime }
+
+func (m *Model) logout() tview.Command {
+	return func() tview.Event {
+		return &LogoutEvent{}
+	}
+}
+
+type QuitEvent struct{ tcell.EventTime }
 
 type closeLayerEvent struct {
 	tcell.EventTime
