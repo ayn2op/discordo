75bf1169ba1c31e3ca45c69d9e082ab642c9c36b
ayn2op <ayn2op@gmail.com>
2026-08-12 01:40:27 -0400

    fix(tls): randomize TLS extension order

--- internal/tls/client.go
+++ internal/tls/client.go
@@ -9,7 +9,7 @@ import (
 )
 
 func NewClient(options ...tls_client.HttpClientOption) tls_client.HttpClient {
-	options = append([]tls_client.HttpClientOption{tls_client.WithClientProfile(clientProfile)}, options...)
+	options = append([]tls_client.HttpClientOption{tls_client.WithClientProfile(clientProfile), tls_client.WithRandomTLSExtensionOrder()}, options...)
 	client, err := tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
 	if err != nil {
 		panic(fmt.Errorf("failed to create new tls http client: %w", err))
